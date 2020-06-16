6bd52aa5764e8bdbf246ad475a1e498b2696c156
andreammgcol <amendez72@misena.edu.co>
2020-06-15 21:10:25 -0500

    fixed spinner location

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,6 +4,7 @@ import '../styles/spinner.css';
 const Spinner = () => {
   return (
     <div className="main">
+        {window.scrollTo(0, 0)}
         <div className="rowPlan"> <h3 class="msj">Shaping your plan</h3> </div>
         <div className="rowPlan">
             <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
